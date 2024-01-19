3338638f420df41a48c7fa382ce11fc1a45695a5
charles <cgiguere@iname.com>
2024-01-18 23:29:46 -0500

    GPIOView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import GPIOView from '@/views/GPIOViewer.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: GPIOView,
       props: route => ({ ...route.query }) // maps all query parameters as props
     },
     {
