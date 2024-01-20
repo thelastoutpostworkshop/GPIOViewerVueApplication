3d2db4f83a25241ca5fe4cb11b99e8b5be4110fb
charles <cgiguere@iname.com>
2024-01-20 15:24:23 -0500

    about

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  showDialog: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(['update:showDialog']);
+
+const closeDialog = () => {
+  emit('update:showDialog', false);
+};
+</script>
+
+<template>
+  <v-dialog v-model="showDialog" width="auto">
+    <v-card>
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" block @click="closeDialog">Close Dialog</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
