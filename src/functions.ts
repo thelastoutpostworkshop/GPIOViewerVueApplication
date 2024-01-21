4a8d45c1fd37e6e15263043844ad3c6fdb637ebf
charles <cgiguere@iname.com>
2024-01-21 11:38:20 -0500

    espinfo

--- src/functions.ts
+++ src/functions.ts
@@ -2,8 +2,21 @@ import { ref, computed } from 'vue';
 import { gpioStore } from '@/stores/gpiostore'
 
 
-export function getAPIUrl(api:string):string {
+export function getAPIUrl(api: string): string {
     const store = gpioStore();
     const url = `http://${store.ipAddress}:${store.httpPort}/${api}`;
     return url;
+}
+
+export function formatBytes(bytes: number | undefined): string {
+    if (bytes) {
+        if (bytes < 1024) {
+            return `${bytes} B`;
+        } else if (bytes < 1024 * 1024) {
+            return `${(bytes / 1024).toFixed(2)} KB`;
+        } else {
+            return `${(bytes / 1024 / 1024).toFixed(2)} MB`;
+        }
+    }
+    return "undefined";
 }
--- src/views/ESPInfo.vue
+++ src/views/ESPInfo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
-import { getAPIUrl } from "@/functions";
+import { getAPIUrl, formatBytes } from "@/functions";
 import type { ESPInfo } from "@/types/types";
 
 const espInfo = ref<ESPInfo>();
@@ -21,5 +21,32 @@ onMounted(() => {
 </script>
 
 <template>
-      {{ espInfo?.chip_model }}
+      <v-table class="mt-10">
+            <tbody>
+                  <tr>
+                        <td>Chip Model</td>
+                        <td>{{ espInfo?.chip_model }}</td>
+                  </tr>
+                  <tr>
+                        <td>Chip Revision</td>
+                        <td>{{ espInfo?.chip_revision }}</td>
+                  </tr>
+                  <tr>
+                        <td>Number of Cores</td>
+                        <td>{{ espInfo?.cores_count }}</td>
+                  </tr>
+                  <tr>
+                        <td>CPU Frequency</td>
+                        <td>{{ espInfo?.cpu_frequency }}Mhz</td>
+                  </tr>
+                  <tr>
+                        <td>Current Cycle Count</td>
+                        <td>{{ espInfo?.cycle_count }}</td>
+                  </tr>
+                  <tr>
+                        <td>Flash Chip Size</td>
+                        <td>{{ formatBytes(espInfo?.flash_chip_size) }}</td>
+                  </tr>
+            </tbody>
+      </v-table>
 </template>
