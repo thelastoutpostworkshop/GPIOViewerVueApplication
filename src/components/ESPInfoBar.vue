7f052bc21e6ba6aa96cfeb26e2b27347acce98f1
charles <cgiguere@iname.com>
2024-01-21 10:50:11 -0500

    esp32 info bar

---
+++ src/components/ESPInfoBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { gpioStore } from '@/stores/gpiostore'
+import { ref } from 'vue';
+
+const store = gpioStore();
+
+</script>
+
+<template>
+    <v-app-bar-title>ESP32 Information</v-app-bar-title>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,29 @@
-import { createRouter, createWebHistory,createMemoryHistory } from 'vue-router'
+import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 import GPIOView from '@/views/GPIOViewer.vue'
 import ESPInfo from '@/views/ESPInfo.vue'
 import GPIOViewBar from '@/components/BoardImageBar.vue'
 import GPIOViewBottomBar from '@/components/BoardImageBottomBar.vue'
+import ESPInfoBar from '@/components/ESPInfoBar.vue'
 
 const router = createRouter({
-  
+
   history: createMemoryHistory(),
   routes: [
-     {
+    {
       path: '/',
-      name:'gpioview',
+      name: 'gpioview',
       components: {
         default: GPIOView,
         "AppBar": GPIOViewBar,
         "BottomBar": GPIOViewBottomBar
       }
     },
-     {
+    {
       path: '/espinfo',
-      name:"espinfo",
+      name: "espinfo",
       components: {
         default: ESPInfo,
+        "AppBar": ESPInfoBar,
 
       }
     },
