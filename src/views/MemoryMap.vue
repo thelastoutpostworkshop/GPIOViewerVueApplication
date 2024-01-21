d5682666b75ab5b853aae0d5492d94028e874448
charles <cgiguere@iname.com>
2024-01-21 18:56:57 -0500

    memory map

--- src/views/MemoryMap.vue
+++ src/views/MemoryMap.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import type { ESPInfo } from "@/types/types";
+import { getAPIUrl, formatBytes } from "@/functions";
 
+const espInfo = ref<ESPInfo>();
+
+async function fetchESPInformation() {
+      try {
+            const response = await fetch(getAPIUrl("espinfo"));
+            const data: ESPInfo = await response.json();
+            espInfo.value = data;
+
+      } catch (error) {
+            console.error("Error fetching esp information", error);
+      }
+}
+
+const totalMemory = computed(() => {
+      return (espInfo.value?.heap_size ?? 0) + (espInfo.value?.psram_size ?? 0);
+});
+
+
+onMounted(() => {
+      fetchESPInformation();
+});
 
 </script>
 
@@ -23,48 +47,59 @@
 
 <style scoped>
 .memory-map {
-    width: 300px;
-    height: 500px;
-    border: 1px solid #000;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end; /* Align sections at the bottom */
+      width: 300px;
+      height: 500px;
+      border: 1px solid #000;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      /* Align sections at the bottom */
 }
 
 .memory-section {
-    width: 70%;
-    height: auto; /* Adjust height based on content */
-    position: relative;
-    border: 1px solid #000;
-    background-color: lightblue;
-    box-sizing: border-box;
-    display: flex; /* Flex container */
-    align-items: flex-end; /* Align used-memory to the bottom */
-    margin-bottom: 10px; /* Optional spacing between sections */
+      width: 70%;
+      height: auto;
+      /* Adjust height based on content */
+      position: relative;
+      border: 1px solid #000;
+      background-color: lightblue;
+      box-sizing: border-box;
+      display: flex;
+      /* Flex container */
+      align-items: flex-end;
+      /* Align used-memory to the bottom */
+      margin-bottom: 10px;
+      /* Optional spacing between sections */
 }
 
 .used-memory {
-    width: 100%;
-    position: absolute;
-    bottom: 0; /* Position at the bottom of the memory-section */
-    background-color: darkblue;
-    color: white;
-    text-align: center;
-    box-sizing: border-box;
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      /* Position at the bottom of the memory-section */
+      background-color: darkblue;
+      color: white;
+      text-align: center;
+      box-sizing: border-box;
 }
 
 .description {
-    width: 30%; /* Width of the description */
-    padding-left: 10px; /* Spacing */
-    box-sizing: border-box;
-    text-align: left;
-    position: absolute; /* Positioned absolutely */
-    right: -35%; /* Adjust as necessary to position outside the memory section */
-    top: 0; /* Align to the top of the parent */
-    bottom: 0; /* Align to the bottom of the parent */
-    display: flex;
-    align-items: center; /* Center text vertically */
+      width: 30%;
+      /* Width of the description */
+      padding-left: 10px;
+      /* Spacing */
+      box-sizing: border-box;
+      text-align: left;
+      position: absolute;
+      /* Positioned absolutely */
+      right: -35%;
+      /* Adjust as necessary to position outside the memory section */
+      top: 0;
+      /* Align to the top of the parent */
+      bottom: 0;
+      /* Align to the bottom of the parent */
+      display: flex;
+      align-items: center;
+      /* Center text vertically */
 }
-
-
 </style>
