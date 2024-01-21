655b8c68a1b3c3fbe2a4561f6d5353b8eff8d99a
charles <cgiguere@iname.com>
2024-01-21 16:00:44 -0500

    about

--- src/components/AboutBar.vue
+++ src/components/AboutBar.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import logo from '@/assets/images/GPIOViewerLogo.png';
 
 </script>
 
 <template>
+    <v-avatar>
+        <v-img :src="logo"></v-img>
+    </v-avatar>
     <v-app-bar-title>About GPIOViewer</v-app-bar-title>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
-import logo from '@/assets/images/GPIOViewerLogo.png';
 
 function goToGithubDiscussion() {
-  window.open('https://github.com/thelastoutpostworkshop/gpio_viewer/discussions', '_blank');
+    window.open('https://github.com/thelastoutpostworkshop/gpio_viewer/discussions', '_blank');
 }
 function goToGithubIssues() {
-  window.open('https://github.com/thelastoutpostworkshop/gpio_viewer/issues', '_blank');
+    window.open('https://github.com/thelastoutpostworkshop/gpio_viewer/issues', '_blank');
 }
 function goToBuyMeACoffee() {
-  window.open('https://www.buymeacoffee.com/thelastoutpostworkshop', '_blank');
+    window.open('https://www.buymeacoffee.com/thelastoutpostworkshop', '_blank');
 }
 </script>
 <template>
-    <v-card class="" subtitle="'Transforms the way you troubleshoot your microcontroller projects" elevation="16">
+    <v-card class="w-75 mt-5" subtitle="A Tool for your ESP32" elevation="16">
         <template v-slot:title>
-            GPIOViewer
-        </template>
-        <template v-slot:prepend>
-            <v-avatar>
-                <v-img :src="logo">
-
-                </v-img>
-            </v-avatar>
+            <div class="text-wrap">Transforms the way you troubleshoot your microcontroller projects</div>
         </template>
         <v-card-text>
             <p class="text-body-1">
@@ -29,19 +21,23 @@ function goToBuyMeACoffee() {
             <v-divider></v-divider>
             <p class="text-body-2 pt-4 d-none d-sm-flex">
                 <v-icon color="blue-darken-4 ">mdi-forum-outline</v-icon>
-                Share Your Story: Join the Discussion<v-icon @click="goToGithubDiscussion">mdi-open-in-new</v-icon>
+                <span class="ml-2 mr-2">Share Your Story: Join the Discussion</span>
+                <v-icon @click="goToGithubDiscussion">mdi-open-in-new</v-icon>
             </p>
             <p class="text-body-2 pt-4 d-flex d-sm-none">
                 <v-icon color="blue-darken-4 ">mdi-forum-outline</v-icon>
-                Share Your Story <v-icon @click="goToGithubDiscussion">mdi-open-in-new</v-icon>
+                <span class="ml-2 mr-2">Share Your Story</span>
+                <v-icon @click="goToGithubDiscussion">mdi-open-in-new</v-icon>
             </p>
             <p class="text-body-2 pt-4 d-none d-sm-flex">
                 <v-icon color="red-darken-4">mdi-alert-circle-outline</v-icon>
-                Report, Track Issues & Request new board<v-icon @click="goToGithubIssues">mdi-open-in-new</v-icon>
+                <span class="ml-2 mr-2">Report, Track Issues & Request new board</span>
+                <v-icon @click="goToGithubIssues">mdi-open-in-new</v-icon>
             </p>
             <p class="text-body-2 pt-4 d-flex d-sm-none">
                 <v-icon color="red-darken-4">mdi-alert-circle-outline</v-icon>
-                Report Issues <v-icon @click="goToGithubIssues">mdi-open-in-new</v-icon>
+                <span class="ml-2 mr-2">Report Issues</span>
+                <v-icon @click="goToGithubIssues">mdi-open-in-new</v-icon>
             </p>
             <p class="pt-6">Please support this project : <v-btn color="yellow" @click="goToBuyMeACoffee">
                     ☕ Buymeacoffee
