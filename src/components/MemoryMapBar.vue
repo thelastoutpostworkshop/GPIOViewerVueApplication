d31f2bc4768c0367a960e14795dfd96956aec458
charles <cgiguere@iname.com>
2024-01-21 18:29:30 -0500

    Memory map

---
+++ src/components/MemoryMapBar.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <v-app-bar-title>Memory Map</v-app-bar-title>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,11 @@ import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 import GPIOView from '@/views/GPIOViewer.vue'
 import ESPInfo from '@/views/ESPInfo.vue'
 import About from '@/views/About.vue'
+import Memory from '@/views/MemoryMap.vue'
 import AboutBar from '@/components/AboutBar.vue'
+import MemoryBar from '@/components/MemoryMapBar.vue'
 import AboutBottomBar from '@/components/AboutBottomBar.vue'
+import MemoryBottomBar from '@/components/MemoryMapBottomBar.vue'
 import GPIOViewBar from '@/components/BoardImageBar.vue'
 import GPIOViewBottomBar from '@/components/BoardImageBottomBar.vue'
 import ESPInfoBar from '@/components/ESPInfoBar.vue'
@@ -38,6 +41,15 @@ const router = createRouter({
         default: About,
         "AppBar": AboutBar,
         "BottomBar": AboutBottomBar
+      }
+    },
+    {
+      path: '/memorymap',
+      name: "memorymap",
+      components: {
+        default: Memory,
+        "AppBar": MemoryBar,
+        "BottomBar": MemoryBottomBar
       }
     },
   ]
