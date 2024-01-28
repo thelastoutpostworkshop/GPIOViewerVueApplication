8530e831c4e29aeef0dd63ee16f901153a7a3574
charles <cgiguere@iname.com>
2024-01-28 14:30:37 -0500

    version 2.0.8

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const store = gpioStore();
 const drawerOpen = ref(false);
 const GPIOViewerRelease = ref("");
 
-const WebApplicationVersion = "2.0.7";
+const WebApplicationVersion = "2.0.8";
 
 declare var window: any;
 
