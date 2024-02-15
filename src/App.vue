9ceec7527a7bbaace4413354832a275b3cc4715f
charles <cgiguere@iname.com>
2024-02-15 06:22:46 -0500

    npm update

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const store = gpioStore();
 const drawerOpen = ref(false);
 const GPIOViewerRelease = ref("");
 
-const WebApplicationVersion = "2.0.8";
+const WebApplicationVersion = "2.0.9";
 
 declare var window: any;
 
