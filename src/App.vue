a12a523fde3c866510340ff3a49091b1fe595af2
charles <cgiguere@iname.com>
2024-03-10 07:42:42 -0400

    npm update

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import { getAPIUrl } from "@/functions";
 const store = gpioStore();
 const drawerOpen = ref(false);
 
-store.WebApplicationRelease = "2.0.9";
+store.WebApplicationRelease = "2.0.10";
 
 declare var window: any;
 
