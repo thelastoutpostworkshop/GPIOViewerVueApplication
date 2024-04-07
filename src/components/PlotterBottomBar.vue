62c34e6385a12256a185cfec7e7cbdb31f5aed0a
charles <cgiguere@iname.com>
2024-04-07 19:08:28 -0400

    freeze

--- src/components/PlotterBottomBar.vue
+++ src/components/PlotterBottomBar.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import logo from '@/assets/images/GPIOViewerLogo.png';
+import { gpioStore } from '@/stores/gpiostore'
 
+const store = gpioStore();
 
 </script>
 
 <template>
+    <v-btn @click="store.freeze = !store.freeze">
+        <v-icon :color="store.freeze ? 'blue' : ''">mdi-snowflake</v-icon>
+        <span>Freeze</span>
+    </v-btn>
     <v-btn icon="mdi-home-automation" @click="$router.push({ name: 'gpioview' })">
         <v-img width="30" :src="logo"></v-img>
         <span>GPIOViewer</span>
