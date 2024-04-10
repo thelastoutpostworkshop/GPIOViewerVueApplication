7ea4a7f38134b7e519859851cc4a6057de5d0b9c
charles <cgiguere@iname.com>
2024-04-10 19:20:53 -0400

    chips

--- src/views/Plotter.vue
+++ src/views/Plotter.vue
@@ -141,7 +141,7 @@ function addDataToDatasetByLabel(chart: ChartData, gpio: number) {
                   </v-card-title>
                   <v-card-text>
                         <v-chip-group column multiple v-model="selectedPins">
-                              <v-chip v-for="pin in pins" :key="pin" :value="pin" color="primary" filter>{{
+                              <v-chip v-for="pin in pins" :key="pin" :value="pin" color="primary" filter size="small">{{
                                     pin.toString() }}</v-chip>
                         </v-chip-group>
 
