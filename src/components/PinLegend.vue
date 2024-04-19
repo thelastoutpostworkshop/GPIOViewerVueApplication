47d0624934d41e231c4cca2e0c5d4e411b5a21f1
charles <cgiguere@iname.com>
2024-04-19 17:24:47 -0400

    legend

---
+++ src/components/PinLegend.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { gpioStore } from '@/stores/gpiostore'
+const store = gpioStore();
+
+
+</script>
+
+<template>
+    <v-card color="secondary">
+        <v-card-title v-if="store.pinTypeDisplay === 1">Legend Pin Types</v-card-title>
+        <v-card-title v-if="store.pinTypeDisplay === 2">Legend Pin Modes</v-card-title>
+        <v-card-text v-if="store.pinTypeDisplay === 1">
+            <div>
+                D = Digital
+            </div>
+            <div>
+                A = Analog
+            </div>
+            <div>
+                P = PWM
+            </div>
+        </v-card-text>
+        <v-card-text v-if="store.pinTypeDisplay === 2">
+            <div>
+                O = Output
+            </div>
+            <div>
+                I = Input
+            </div>
+        </v-card-text>
+    </v-card>
+
+</template>
--- src/views/GPIOViewer.vue
+++ src/views/GPIOViewer.vue
@@ -2,11 +2,15 @@
 import { ref, computed, onMounted } from 'vue';
 import BoardImage from '@/components/BoardImage.vue';
 import { gpioStore } from '@/stores/gpiostore'
+import PinLegend from '@/components/PinLegend.vue';
 
 const store = gpioStore();
 
 </script>
 
 <template>
-      <BoardImage :board="store.currentBoard"></BoardImage>
+      <div>
+            <BoardImage :board="store.currentBoard"></BoardImage>
+            <PinLegend></PinLegend>
+      </div>
 </template>
