dbca9173805c50ac91b976230b0d38aad432243a
charles <cgiguere@iname.com>
2024-04-19 19:18:56 -0400

    pin legends

--- src/components/BoardImageBar.vue
+++ src/components/BoardImageBar.vue
@@ -11,5 +11,41 @@ const menu = ref(false);
 <template>
     <BoardSelect v-if="store.boards" :boards="store.boards" />
     <v-spacer></v-spacer>
-    <div class="pa-5">@{{ store.SamplingInterval }}ms</div>
+    <v-btn icon :disabled="store.pinTypeDisplay === 0">
+        <v-icon>mdi-tooltip</v-icon>
+        <v-menu activator="parent" location="bottom end" transition="fade-transition">
+            <v-list density="compact" rounded="lg" slim>
+                <v-list-item v-if="store.pinTypeDisplay === 1" prepend-icon="mdi-pin" title="Pin Types"
+                    link></v-list-item>
+                <v-list-item v-if="store.pinTypeDisplay === 2" prepend-icon="mdi-pin" title="Pin Modes"
+                    link></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <div v-if="store.pinTypeDisplay === 1">
+                    <v-list-item min-height="24">
+                        D = Digital
+                    </v-list-item>
+                    <v-list-item min-height="24">
+                        A = Analog
+                    </v-list-item>
+                    <v-list-item min-height="24">
+                        P = PWM
+                    </v-list-item>
+                </div>
+                <div v-if="store.pinTypeDisplay === 2">
+                    <v-list-item min-height="24">
+                        O = Output
+                    </v-list-item>
+                    <v-list-item min-height="24">
+                        I = Input
+                    </v-list-item>
+                    <v-list-item min-height="24">
+                        - = Not Set
+                    </v-list-item>
+                </div>
+            </v-list>
+        </v-menu>
+    </v-btn>
+    <div class="pr-2">@{{ store.SamplingInterval }}ms</div>
 </template>
--- src/views/GPIOViewer.vue
+++ src/views/GPIOViewer.vue
@@ -2,15 +2,11 @@
 import { ref, computed, onMounted } from 'vue';
 import BoardImage from '@/components/BoardImage.vue';
 import { gpioStore } from '@/stores/gpiostore'
-import PinLegend from '@/components/PinLegend.vue';
 
 const store = gpioStore();
 
 </script>
 
 <template>
-      <div>
             <BoardImage :board="store.currentBoard"></BoardImage>
-            <PinLegend></PinLegend>
-      </div>
 </template>
