76508c24152e20de72b915c60d2c381d363defca
charles <cgiguere@iname.com>
2024-12-24 07:33:52 -0500

    functions

--- src/components/BoardSelect.vue
+++ src/components/BoardSelect.vue
@@ -5,6 +5,7 @@ import { VSelect } from 'vuetify/components';
 import { gpioStore } from '@/stores/gpiostore'
 
 import type { BoardData } from "../types/types";
+import { getCookie, setCookie } from '@/functions';
 
 const store = gpioStore();
 const props = defineProps({
@@ -40,26 +41,6 @@ onMounted(() => {
     }
 });
 
-function setCookie(name: string, value: string, days?: number): void {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name: string): string | null {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === " ") c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
 
 </script>
 <template>
