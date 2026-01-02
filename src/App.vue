c3fc4d5a3b7542a8edc5b713ec920c8d13d68257
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2026-01-02 11:48:52 -0500

    v2.2.7

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ const maxLastPinValuesStored = 100;
 const router = useRouter()
 const theme = useTheme();
 
-store.WebApplicationRelease = "2.2.6";
+store.WebApplicationRelease = "2.2.7";
 
 declare var window: any;
 function getTheme() {
